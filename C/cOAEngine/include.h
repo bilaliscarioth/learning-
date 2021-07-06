9e239b86c4a51236192449064cb6e5b349f326eb
bilaliscarioth <mrvxo@protonmail.com>
2021-07-06 05:35:38 +0200

    add glfw

--- C/cOAEngine/include.h
+++ C/cOAEngine/include.h
@@ -1,8 +1,26 @@
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/Xos.h>
-
+#include <GLFW/glfw3.h>
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+#include <glad/glad.h>
+#include <GL/glew.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-void x_init();
+#define MAX_SPEED 0.4
+#define ZOOM_SPEED 0.1
+#define SPEED 0.1
+
+struct Vector3D {
+	float x, y, z;
+} Vector3D;
+
+struct Vector3D createVector3D(float x, float y, float z){
+	struct Vector3D temp;
+	temp.x = x;
+	temp.y = y;
+	temp.z = z;
+	return temp;
+};
+
+void close();
+void framebufferSizeCallback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
--- C/cOAEngine/main.c
+++ C/cOAEngine/main.c
@@ -1,46 +1,33 @@
-#include "include.h"
+#include "./include.h"
+const int width = 800, height = 600;
 
-Display *disp;
-Window win;
-int screen;
-GC gc;
+void framebufferSizeCallback(GLFWwindow *window, int width, int height){
+	glViewport(0,0, width, height);
+}
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, 1);
+}
 
-void x_init(){
-	unsigned long black, white;
+int main(int argc, char **argv){
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow *window = glfwCreateWindow(width, height, "LyokoWorld", NULL, NULL);	
 
-	disp = XOpenDisplay(NULL);
-	if (!disp){
-		fprintf(stderr , "Display unknown");	
-		exit(1);
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	while(!glfwWindowShouldClose(window)){
+		processInput(window);
+		
+		//glClearColor(0.2f, 0.3f, 0.3f, 1.0F);
+		//glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
-	screen = DefaultScreen(disp);
-
-	black = BlackPixel(disp, screen);
-	white = WhitePixel(disp, screen);
-
-	win = XCreateSimpleWindow(disp, DefaultRootWindow(disp), 0,0, 800, 600, 5, white, black);
-	XSetStandardProperties(disp, win, "cOA Engine", "v001", None, NULL, 0, NULL);
-
-	XSelectInput(disp, win, ExposureMask|ButtonPressMask|KeyPressMask);
-	gc=XCreateGC(disp, win, 0,0);
-	
-	XSetBackground(disp, gc, black);
-	XSetForeground(disp, gc, white);
-
-	XMapWindow(disp, win);
-
-};
-
-int main() {
-	x_init();
- 	XEvent e;
-	while (1) {
-		XNextEvent(disp, &e);
-		if (e.type == KeyPress)	
-			XDrawRectangle(disp, win, gc, 200, 300, 100, 100);
-		//break;
-   }
-	XClearWindow(disp, win);
-	XMapRaised(disp, win);
+	return 0;
 }
